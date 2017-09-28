77a81a071c41869de089b167b034531458c48bbe
Rodrigo Chibana <s2it_rchibana@uolinc.com>
2017-09-28 13:00:36 -0300

    first commit

---
+++ src/main/java/br/com/multipledatasources/Application.java
@@ -0,0 +1,19 @@
+package br.com.multipledatasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author s2it_rchibana
+ * @version $Revision: $<br/>
+ *          $Id: $
+ * @since 9/28/17 12:36 PM
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class, args);
+    }
+
+}
